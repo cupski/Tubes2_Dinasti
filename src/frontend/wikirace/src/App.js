2e889f2fe2980518deb681eb785c3951a02a6794
denoseu <denisefeliciatiowanni@gmail.com>
2024-04-10 00:40:14 +0700

    feat: tempat button

--- src/frontend/wikirace/src/App.js
+++ src/frontend/wikirace/src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/header";
+import ButtonContainer from "./components/buttoncontainer";
 
 function App() {
   return (
     <div className="app-container">
       <Header></Header>
+      <ButtonContainer />
     </div>
   );
 }
