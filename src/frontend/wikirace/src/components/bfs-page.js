fc48d5a932b0e50d6eb93cea97a6be7500e44941
denoseu <denisefeliciatiowanni@gmail.com>
2024-04-12 11:51:31 +0700

    feat: input

--- src/frontend/wikirace/src/components/bfs-page.js
+++ src/frontend/wikirace/src/components/bfs-page.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './styles.css';
 
 const BFSPage = () => {
+    const [startPoint, setStartPoint] = useState("");
+    const [endPoint, setEndPoint] = useState("");
+    const [result, setResult] = useState("");
+
+    const handleSearch = () => {
+        // seach logic disini, masih bingung.
+        setResult(`Results for BFS with start point "${startPoint}" and end point "${endPoint}"`);
+    };
+
     return (
-        <div>
+        <div className="logic-container">
             <h1>BFS</h1>
-            <p>nanti terima input start point, end point, dan hasil keluar disini</p>
+            <div>
+                <input type="text" value={startPoint} onChange={(e) => setStartPoint(e.target.value)} placeholder="Start Point" />
+            </div>
+            <div>
+                <input type="text" value={endPoint} onChange={(e) => setEndPoint(e.target.value)} placeholder="End Point" />
+            </div>
+            <div>
+                <button onClick={handleSearch}>Search</button>
+            </div>
+            <div>
+                <p>{result}</p>
+            </div>
         </div>
     );
 }
