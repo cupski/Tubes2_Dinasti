74325e5725bc87b79431a01ac190095d2e90b5ad
denoseu <denisefeliciatiowanni@gmail.com>
2024-04-18 15:30:02 +0700

    feat: button container

--- src/frontend/wikirace/src/components/header.js
+++ src/frontend/wikirace/src/components/header.js
@@ -7,10 +7,21 @@ const Header = () => {
     return (
         <div className='header-container'>
             <div className='max-width-container'>
-            <img src={title} alt="Wikirace by Dinasti" className='header-wikirace'/>
+                <img src={title} alt="Wikirace by Dinasti" className='header-wikirace'/>
                 <img src={text} alt="one piece text" className='header-image'/>
+                
+                <div className="information-button-container">
+                    <button className="header-button">
+                        How to Use
+                    </button>
+                    <button className="header-button">
+                        About Us
+                    </button>
+                </div>
             </div>
+
         </div>
+
     );
 }
 
